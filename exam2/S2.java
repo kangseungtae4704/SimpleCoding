4872b08f807b197fce4d43611ef8680763dc5b3e
kangseungtae4704 <jintmdxo@naver.com>
2025-05-29 17:42:34 +0900

    Add files via upload

---
+++ exam2/S2.java
@@ -0,0 +1,12 @@
+package exam2;
+
+public class S2 {
+    public static void main(String[] args) {
+    	int a=19;
+		if (a>-19) {
+			System.out.println("성인");
+		} else {
+              System.out.println("미성년자");
+		}
+	}
+}
---
+++ exam2/S3.java
@@ -0,0 +1,18 @@
+package exam2;
+
+import java.util.Scanner;
+
+public class S3 {
+	public static void main(String[] args) {
+	
+	Scanner a = new Scanner(System.in);
+	int b=a.nextInt();
+	System.out.println(b-4);
+	System.out.println(b-3);
+	System.out.println(b-2);
+	System.out.println(b-1);
+	System.out.println(b);
+  
+    
+}
+}
